4756a12a55a214fe2995a63521a1791cb481e547
Aleksei Sushkov <903434@gmail.com>
2025-07-27 21:29:27 +0100

    setup upd 2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import dts from "vite-plugin-dts"
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    svgr(),
-  ],
+  plugins: [react(), svgr()],
   build: {
     lib: {
       entry: "src/index.tsx",
