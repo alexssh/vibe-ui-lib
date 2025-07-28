aff6c9a66aefcd1bdde2ce5cff9db81bfe941e78
Aleksei Sushkov <903434@gmail.com>
2025-07-28 21:00:37 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  env: { browser: true, es2017: true },
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
