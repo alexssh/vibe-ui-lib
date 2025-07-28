9f2cb5dff65f3907ea4ed30dd4f9ae6280eef59b
Aleksei Sushkov <903434@gmail.com>
2025-07-28 19:26:19 +0100

    tailwind env

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   build: {
     lib: {
-      entry: "src/index.tsx",
+      entry: "src/index.ts",
       name: "vibe-ui-lib",
       formats: ["es"],
       fileName: (format) => `index.${format}.js`,
