a533e41a169c32c87675a03618d7f86774e02af4
Aleksei Sushkov <903434@gmail.com>
2025-07-28 19:25:48 +0100

    storybook tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,16 @@ import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [],
+  addons: [
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
