a506f3cd97ba7a85adc08f874c77cbad3829acaf
Aleksei Sushkov <903434@gmail.com>
2025-08-11 11:36:27 +0100

    styles

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import "./styles.css"
+import "./style.css"
 
 export { Example } from "./components/Example/Example"
 export type { ExampleProps } from "./components/Example/Example.types"
+export { Icon } from "./components/Icon"
+export type { IconProps } from "./components/Icon/Icon.types"
